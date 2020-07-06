21c30f8ddffda34ab901ff4ca6374288165e3bee
aselyacan <iskender1982@gmail.com>
2020-07-06 20:02:28 +0200

    ilk commit local repo den to remote repo icin

--- src/git_dunya/Dunya.java
+++ src/git_dunya/Dunya.java
@@ -3,8 +3,9 @@ package git_dunya;
 public class Dunya {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("day1");
+		System.out.println("day2");
 	}
 
 }
