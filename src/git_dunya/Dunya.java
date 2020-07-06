4118233b47633a0d2a4f2145c4339dd5a78f4fee
aselyacan <iskender1982@gmail.com>
2020-07-06 20:13:06 +0200

    ikinci commit for  day2 branch icin  from local  repo dan

--- src/git_dunya/Dunya.java
+++ src/git_dunya/Dunya.java
@@ -8,6 +8,8 @@ public class Dunya {
 		System.out.println("day2");
 		System.out.println("day2 branchi olusturuldu ve bunu master olarak kabul edin");
 		System.out.println("duzenlemeleri bunda yapin");
+		System.out.println("Day2 icin ilk deneme yazildi artik ");
+		System.out.println("ikinci commit  local rep to remot day2 branch icin ");
 		
 	}
 
